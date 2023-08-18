090609390ee2ca3c95bbd8b501dd82f51e9d7542
Mihir Patel <mihir.fluxbyte@gmail.com>
2023-08-19 03:44:39 +0530

    Migrated code from js to ts and updated some logic

---
+++ api/proxy.ts
@@ -0,0 +1,56 @@
+// api/proxy.js
+// import fetch from 'node-fetch';
+import fetch from 'isomorphic-unfetch';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const { method, headers, body, url } = req;
+  
+  // const backendUrl = 'http://ec2-54-198-132-96.compute-1.amazonaws.com/api'; // Replace with your backend URL
+  // const apiUrl = `${backendUrl}${url?.replace('/api/proxy', '')}`
+  
+  const path = url?.replace('/api/proxy/', '') ?? ''; // Call api with /api/proxy/http://ec2-54-198-132-96.compute-1.amazonaws.com/api
+    // Extract the domain from the path
+  const domainMatch = path.match(/^(https?:\/\/.+)$/);
+
+  if (!domainMatch) {
+    res.status(400).json({ error: 'Invalid URL format' });
+    return; 
+  }
+
+  const apiUrl = domainMatch[0];
+  console.log({ method, headers, body, url, apiUrl});
+
+  // causing error so need to delete
+  delete headers['transfer-encoding'];
+
+  try {
+    const response = await fetch(apiUrl, {
+      method,
+      headers: headers as any,
+      body: method !== 'GET' ? JSON.stringify(body) : undefined,
+    });
+
+    console.log({response})
+    const responseBody = await response.text();
+    res.status(response.status).send(responseBody)
+
+    // if (!response.ok) {
+    //   // Get the original error status code from the backend response
+    //   const originalStatusCode = response.status;
+    //   res.status(originalStatusCode).send(responseBody);
+    //   return;
+    // }
+
+    // try {
+    //   const jsonResponseData = JSON.parse(responseBody);
+    //   res.status(response.status).json(jsonResponseData);
+    // } catch (error) {
+    //   console.error('JSON parse error:', error);
+    //   res.status(500).json({ error: 'An error occurred while processing the response.' });
+    // }
+  } catch (error) {
+    console.error('Proxy error:', error);
+    res.status(500).json({ error: 'An error occurred while proxying the request.' });
+  }
+};
